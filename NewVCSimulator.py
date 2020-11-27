4743fd0c552bb1debac8d4035508b4c74bd2f0f0
Beau Horenberger <horenbergerbeau@gmail.com>
2020-11-27 13:05:22 -0500

    changed queue structure

--- NewVCSimulator.py
+++ NewVCSimulator.py
@@ -26,6 +26,15 @@ DIM1 = 2
 DIM2 = 2
 PORTS = 4
 
+###########
+#CONSTANTS#
+###########
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+DIRS = [UP, RIGHT, DOWN, LEFT]
+
 ##################
 #ABSTRACT CLASSES#
 ##################
@@ -44,6 +53,8 @@ class Container():
     def putI(self, package):
         self.Ibuffer = package
         package.moved = True
+        if isinstance(package, Header):
+            package.parsed = False
 
     #puts an item in the output buffer, moves to storage if buffer is full
     def putO(self, package):
@@ -52,6 +63,7 @@ class Container():
             package.moved = True
         else:
             self.Ostorage.append(package)
+            package.moved = True
 
     #looks at the item in the buffer
     def peekI(self):
@@ -88,7 +100,15 @@ class Container():
 #########
 #CLASSES#
 #########
-    
+
+#single instruction for a router's instruction queues
+class Instruction:
+    def __init__(self, source, dest, amount, time):
+        self.source = source
+        self.dest = dest
+        self.amount = amount
+        self.time = time
+
 class Flit:
     def __init__(self):
         self.moved = False
@@ -98,6 +118,7 @@ class Header:
         self.dest = dest
         self.length = length
         self.moved = False
+        self.parsed = False
         
 class Processor(Container):
     def __init__(self):
@@ -106,7 +127,7 @@ class Processor(Container):
         self.Ostorage = []
         #instructions are the memory that tells us whether we're in the middle of moving a message
         #they are read by the Router and used to direct movement from the processor OBuffer to a port OBuffer
-        self.instructions = None
+        self.instructions = []
 
     #generates new msgs in processor; either sends straight to Obuffer or puts in storage
     #NOTE: Never sends message to itself
@@ -127,13 +148,10 @@ class Processor(Container):
 
     #move is called many times per unit time
     def move(self):
-        moved = 0
         #munch those inputs
-        if self.Ibuffer != None and not self.Ibuffer.moved:
+        if self.Ibuffer != None:
             self.Ibuffer = None
-            moved = 1
 
-        return moved
 
     #step is called exactly once per unit time
     def step(self, address):
@@ -151,13 +169,14 @@ class Port(Container):
         #instructions are the memory that tells us whether we're currently in the middle of transmitting a message through this port
         #set to [X, Y] to move Y flits from Ibuffer to port X OStorage
         #X=-1 means to send it to processor (i.e. we've arrived at destination)
-        self.instructions = None    
+        self.instructions = []   
                     
 #receives messages from other routers in input ports, redirects to output ports or processor based on the header destination
 class Router:
     def __init__(self, address, ports = PORTS):
         #address (used for header parsing and in router's processor for message generation)
         self.address = address
+        self.time = 0
 
         #the four ports of the router
         #convention: [0,1,2,3] = [UP,RIGHT,DOWN,LEFT]
@@ -202,17 +221,20 @@ class Router:
         col_width = max(len(word) for row in [port_list, line] for word in row) + 2
         for row in [port_list, line]:
             print("".join(word.ljust(col_width) for word in row))
-
             
     #calculates instructions for port or processor; used when a new header appears in an Obuffer
-    def parse_header(self, cur):
-        instructions = [-1, cur.length]
+    def parse_header(self, cur, source):
+        instruction = Instruction(source, -1, cur.length, self.time)
+        #instruction = [source, -1, cur.length, self.time]
+
+        #mark this header as parsed
+        cur.parsed = True
         #find best choice out of l/r (denoted false/true) options
         lr_dir = -1
         if ((self.address[1]-cur.dest[1])%DIM1 > (cur.dest[1]-self.address[1])%DIM1):
-            lr_dir = 1
+            lr_dir = RIGHT
         else:
-            lr_dir = 3
+            lr_dir = LEFT
         #note if we're already done moving l/r
         if self.address[1] == cur.dest[1]:
             lr_dir = -1
@@ -220,133 +242,125 @@ class Router:
         #find best choice out of up/down (denoted false/true) options
         ud_dir = -1
         if ((self.address[0]-cur.dest[0])%DIM2 > (cur.dest[0]-self.address[0])%DIM2):
-            ud_dir = 0
+            ud_dir = UP
         else:
-            ud_dir = 2
+            ud_dir = DOWN
         #note if we're already done moving up/down
         if self.address[0] == cur.dest[0]:
             ud_dir = -1
 
         #if there's only one choice, return it
         if lr_dir == -1:
-            instructions[0] = ud_dir
-            return instructions
+            instruction.dest = ud_dir
+            return instruction
         if ud_dir == -1:
-            instructions[0] = lr_dir
-            return instructions
+            instruction.dest = lr_dir
+            return instruction
     
-            
         #pick choice with shortest queue
         if (len(self.ports[lr_dir].Ostorage) +(self.ports[lr_dir].Obuffer != None)) < (len(self.ports[ud_dir].Ostorage) + (self.ports[ud_dir].Obuffer != None)):
-            instructions[0] = lr_dir
+            instruction.dest = lr_dir
         else:
-            #tie breaker goes with ud_dir
-            instructions[0] = ud_dir
+            #tie breaker goes with smallest port number
+            instruction.dest = min(lr_dir, ud_dir)
         print("L/R: {}, U/D: {}".format(lr_dir, ud_dir))
-        print("Chosen port: {}".format(instructions[0]))
-        return instructions
+        print("Chosen port: {}".format(instruction.dest))
+        return instruction
 
-    #called many times per time unit; all about moving flits from IBuffers to OBuffers or to processor
+    #all about moving flits from IBuffers to OBuffers or to processor
     def move(self):
-        moved = 0
+        #internal processor movement (Munching inputs)
+        self.processor.move()
+        #incrementing the queues of each output port
+        for port in self.ports:
+            if len(port.instructions) > 0:
+                #pop old instructions
+                if port.instructions[0].amount == 0:
+                    port.instructions.pop(0)
+            #if we still have instructions, try to take action
+            if len(port.instructions) > 0:
+                #flit to be grabbed
+                if port.instructions[0].source != -1:
+                    target = self.ports[port.instructions[0].source].peekI()
+                else:
+                    target = self.processor.peekO()
+                if target != None and not target.moved:
+                    #moving to another port
+                    if port.instructions[0].source != -1:
+                        port.putO(self.ports[port.instructions[0].source].getI())
+                    else:
+                        port.putO(self.processor.getO())
+                    #moving to processor
+                    #decrement amount
+                    port.instructions[0].amount -= 1
+        #incrementing processor queue
+        if self.processor.peekI() == None:
+            if len(self.processor.instructions) > 0:
+                if self.processor.instructions[0].amount == 0:
+                    self.processor.instructions.pop(0)
+            if len(self.processor.instructions) > 0:
+                if self.processor.peekI() == None:
+                    self.processor.putI(self.ports[self.processor.instructions[0].source].getI())
+                    self.processor.instructions[0].amount -= 1
+            
+    #looks at all the new inputs and creates instructions
+    def parse_all_inputs(self):
         #################
         #PROCESSOR LOGIC#
         #################
-        #internal processor movement (Munching inputs)
-        moved = self.processor.move()
 
         #if our processor has a header in OBuffer, parse it into processor instructions
         cur = self.processor.peekO()
-        if isinstance(cur, Header) and not cur.moved:
-            new_instructions = self.parse_header(cur)
-            #IMPORTANT! Only give the processor instructions if no other port is already transporting flits to destination port
-            locked = False
-            for x in range(0, len(self.ports)):
-                if self.ports[x].instructions != None and self.ports[x].instructions[0] == new_instructions[0] and self.ports[x].instructions[1] > 0:
-                    locked = True
-                    break
-            if not locked:
-                self.processor.instructions = self.parse_header(cur)
-
-        #if our processor has instructions from a parsed header, try to sent the contents of the out buffer to the desired port
-        if self.processor.instructions != None:
-            #print("Found Instructions for processor output in router ({},{}): ({},{})".format(*(self.address+self.processor.instructions)))
-            if self.processor.instructions[1] > 0 and self.processor.peekO() != None and not self.processor.peekO().moved:
-                self.ports[self.processor.instructions[0]].putO(self.processor.getO())
-                self.processor.instructions[1] -= 1
-                moved = 1
-            elif self.processor.instructions[1] <= 0:
-                self.processor.instructions = None
-
+        if isinstance(cur, Header) and not (cur.parsed or cur.moved):
+            new_instr = self.parse_header(cur, -1)
+            self.ports[new_instr.dest].instructions.append(new_instr)
+        
         ############
         #PORT LOGIC#
         ############
-        #moving the contents of INPUT buffer in ports
+        #parsing the contents of input ports into instructions
         counter = 0
-        for port in self.ports:
-            #If we have already parsed a header and have instructions:
-            if port.instructions != None:
-                #print("Found Instructions for port {} in router ({},{}): ({},{})".format(*([counter]+self.address+port.instructions)))
-                #flits which have reached their destination go to processor
-                if port.instructions[0] == -1 and port.instructions[1] > 0:
-                    if self.processor.peekI() == None and port.peekI() != None and not port.peekI().moved:
-                        self.processor.putI(port.getI())
-                        moved = 1
-                        port.instructions[1] -= 1
-                #otherwise just send the flits along
-                elif port.instructions[1] > 0 and port.peekI() != None and not port.peekI().moved:
-                    self.ports[port.instructions[0]].putO(port.getI())
-                    moved = 1
-                    port.instructions[1] -= 1
-                #erase instructions if we've moved all the expected flits
-                elif port.instructions[1] <= 0:
-                    print("   WIPING INSTRUCTIONS")
-                    port.instructions = None
+        for port in range(len(self.ports)):
+            cur = self.ports[port].peekI()
+            #NOTE: Parsing the header here takes up one flit, which is why moving a header takes 2 time units
+            if isinstance(cur, Header) and not (cur.moved or cur.parsed):
+                #eat headers that have arrived at their dest
+                if cur.dest == self.address:
+                    self.processor.instructions.append(Instruction(port, -1, cur.length, self.time))
+                    cur.moved = True
+                else:
+                    new_instr = self.parse_header(cur, port)
+                    self.ports[new_instr.dest].instructions.append(new_instr)
+                    cur.moved = True
 
-            #otherwise we expect either a header or nothing
-            cur = port.peekI()
-            if cur != None and port.instructions == None:
-                #NOTE: Parsing the header here takes up one flit, which is why moving a header takes 2 time units
-                if isinstance(cur, Header) and not cur.moved:
-                    #eat headers that have arrived at their dest
-                    if cur.dest == self.address:
-                        port.instructions = [-1, cur.length]
-                        cur.moved = True
-                        moved = 1
-                    else:
-                        new_instructions = self.parse_header(cur)
-                        locked = False
-                        for x in range(0, len(self.ports)):
-                            if self.ports[x].instructions != None and self.ports[x].instructions[0] == new_instructions[0] and self.ports[x].instructions[1] > 0:
-                                locked = True
-                                break
-                        if not locked:
-                            port.instructions = self.parse_header(cur)
-                            cur.moved = True
-                            moved = 1
-                    
-                if isinstance(cur, Flit):
-                    raise Exception("Rogue flit in port of router [{},{}]".format(*(self.address)))
             counter += 1
             
-        return moved
 
-    #takes router id
-    def step(self):
+
+    def step(self, time):
+        self.time = time
+
         #step processor
         self.processor.step(self.address)
 
+        self.parse_all_inputs()
+        
+        self.move()
+
+    def reset_moved(self):
         #reset all flits
         self.processor.reset_moved()
         for port in self.ports:
             port.reset_moved()
+        
     
 class TwoDToroidalNetwork:
     #routers in the self.routers matrix are connected to adjacent routers
     #connections apply modulo dim1 and dim2 (because it's a toroidal network)
     def __init__(self):
         random.seed()
-                
+        self.time = 0
+        
         #table of routers
         self.routers = []
         for x in range(0, DIM1):
@@ -358,41 +372,33 @@ class TwoDToroidalNetwork:
 
     def step(self):
         #part 1: iterate over all routers, continue moving until there's nothign left to move
-        moving = 1
-        while moving > 0:
-            moving = 0
-            for i in range(0, len(self.routers)):
-                for j in range(0, len(self.routers[0])):
-                    #inter-router movement
-                    moving += self.routers[i][j].move()
-
-                    #intra-router movement
-                    #[i][j] is rowsxcolumns
-                    for p in range(0, PORTS):
-                        if self.routers[i][j].ports[p].peekO() != None and not self.routers[i][j].ports[p].peekO().moved:
-                            if p == 0:
-                                if self.routers[(i-1)%DIM1][j].ports[2].peekI() == None:
-                                    moving += 1
-                                    self.routers[(i-1)%DIM1][j].ports[2].putI(self.routers[i][j].ports[p].getO())
-                            elif p == 1:
-                                if self.routers[i][(j+1)%DIM2].ports[3].peekI() == None:
-                                    moving += 1
-                                    self.routers[i][(j+1)%DIM2].ports[3].putI(self.routers[i][j].ports[p].getO())
-                            elif p == 2:
-                                if self.routers[(i+1)%DIM1][j].ports[0].peekI() == None:
-                                    moving += 1
-                                    self.routers[(i+1)%DIM1][j].ports[0].putI(self.routers[i][j].ports[p].getO())
-                            elif p == 3:
-                                if self.routers[i][(j-1)%DIM2].ports[1].peekI() == None:
-                                    moving += 1
-                                    self.routers[i][(j-1)%DIM2].ports[1].putI(self.routers[i][j].ports[p].getO())
-                            
-        #(electric boogaloo) part 2: step all the routers internally
+        moving = 0
         for i in range(0, len(self.routers)):
             for j in range(0, len(self.routers[0])):
-                self.routers[i][j].step()
+                #inter-router movement
+                self.routers[i][j].step(self.time)
+
+                #intra-router movement
+                #[i][j] is rowsxcolumns
+                for p in DIRS:
+                    if self.routers[i][j].ports[p].peekO() != None and not self.routers[i][j].ports[p].peekO().moved:
+                        if p == UP:
+                            if self.routers[(i-1)%DIM1][j].ports[DOWN].peekI() == None:
+                                self.routers[(i-1)%DIM1][j].ports[DOWN].putI(self.routers[i][j].ports[p].getO())
+                        elif p == RIGHT:
+                            if self.routers[i][(j+1)%DIM2].ports[LEFT].peekI() == None:
+                                self.routers[i][(j+1)%DIM2].ports[LEFT].putI(self.routers[i][j].ports[p].getO())
+                        elif p == DOWN:
+                            if self.routers[(i+1)%DIM1][j].ports[UP].peekI() == None:
+                                self.routers[(i+1)%DIM1][j].ports[UP].putI(self.routers[i][j].ports[p].getO())
+                        elif p == LEFT:
+                            if self.routers[i][(j-1)%DIM2].ports[RIGHT].peekI() == None:
+                                self.routers[i][(j-1)%DIM2].ports[RIGHT].putI(self.routers[i][j].ports[p].getO())
+
+                self.routers[i][j].reset_moved()
                 self.routers[i][j].debug()
 
+        self.time += 1
 
 test_net = TwoDToroidalNetwork()
 
